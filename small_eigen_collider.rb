ded63dd95dbfdad29cbf2d4fd29f9fd3427cfdaa
Andrew Grimm <andrew.j.grimm@gmail.com>
2010-10-24 22:57:55 +1100

    Created classes Task and TaskCreator.

--- small_eigen_collider.rb
+++ small_eigen_collider.rb
@@ -6,20 +6,6 @@ require "timeout"
 # # Module for logging
 # # A monkey-patched method that'd enable an object id and implementation-independent representation of an object
 
-# FIXME make this configurable
-srand(42)
-
-# FIXME make a better way of creating random objects
-objects = []
-file = File.open("random_text.txt")
-100.times do
-  objects << file.read(rand(20))
-end
-100.times do
-  objects << rand(10)
-end
-objects << " "
-
 # FIXME make a better way to log things
 if defined?(RUBY_ENGINE)
   output_filename = "#{RUBY_ENGINE}_#{RUBY_VERSION}_output.txt"
@@ -56,10 +42,10 @@ class SmallEigenCollider::Logger
     @filestream.puts "Start"
   end
 
-  def log_input_parameters(receiver_object, method, parameter_objects)
-    @filestream.puts "Receiver object: " + receiver_object.inspect
-    @filestream.puts "Method: " + method.inspect
-    @filestream.puts "Parameters: " + parameter_objects.inspect
+  def log_input_parameters(task)
+    @filestream.puts "Receiver object: " + task.receiver_object.inspect
+    @filestream.puts "Method: " + task.method.inspect
+    @filestream.puts "Parameters: " + task.parameter_objects.inspect
   end
 
   def log_result(result)
@@ -80,40 +66,94 @@ class SmallEigenCollider::Logger
   end
 end
 
-logger = SmallEigenCollider::Logger.new_using_filename(output_filename)
+class SmallEigenCollider::TaskCreator
+  def initialize
+    # FIXME make this configurable
+    srand(42)
 
-# FIXME replace this whitelist of non-risky (I hope!) methods with something more flexible.
-# I can get the list of methods using reflection, but how do I ensure that any operations won't delete the root directory?
-methods = (["insert", "include?", "gsub", "size", "replace", "to_i", "chomp!", "succ", "oct", "to_s", "rstrip", "taguri=", "lines", "capitalize!", "hash", "capitalize", "center", "*", "index", "crypt", "+", "=~", "strip", "each_byte", "gsub!", "count", "delete!", "upcase", "ljust", "delete", "is_binary_data?", "upcase!", "rstrip!", "sum", "eql?", "start_with?", "to_sym", "length", "chop", "to_yaml", "to_f", "tr!", "to_str", "[]", "unpack", "tr", "inspect", "bytes", "strip!", "[]=", "slice!", "split", "sub", "each", "empty?", "swapcase!", "<<", "casecmp", "swapcase", "rindex", "intern", "rpartition", "reverse!", "next!", "lstrip", "hex", "chop!", "match", "each_char", "downcase!", "rjust", "downcase", "squeeze", "squeeze!", "concat", "upto", "end_with?", "slice", "chomp", "<=>", "bytesize", "sub!", "succ!", "each_line", "dump", "==", "scan", "tr_s", "tr_s!", "partition", "is_complex_yaml?", "next", "%", "reverse", "lstrip!", "chars", "taguri"] + ["%", "odd?", "prec_i", "<<", "div", "&", ">>", "lcm", "power!", "to_sym", "*", "ord", "+", "next", "round", "prec_f", "-", "even?", "denominator", "singleton_method_added", "divmod", "/", "integer?", "downto", "gcdlcm", "|", "gcd", "size", "truncate", "~", "to_i", "modulo", "zero?", "times", "to_r", "rdiv", "^", "+@", "-@", "quo", "**", "upto", "to_f", "<", "step", "numerator", "<=>", "between?", "remainder", ">", "to_int", "nonzero?", "pred", "coerce", "rpower", "floor", "succ", ">=", "prec", "<=", "fdiv", "abs", "chr", "id2name", "ceil", "[]"]).uniq
-10000.times do
-  receiver_object_index = rand(objects.size)
-  num_parameters = rand(5)
-  parameters_indexes = []
-  num_parameters.times do
-    index = rand(objects.size)
-    next if index == receiver_object_index
-    next if parameters_indexes.include?(index)
-    parameters_indexes << index
+    # FIXME make a better way of creating random objects
+    @objects = []
+    file = File.open("random_text.txt")
+    100.times do
+      @objects << file.read(rand(20))
+    end
+    100.times do
+      @objects << rand(10)
+    end
+    @objects << " "
+
+    # FIXME replace this whitelist of non-risky (I hope!) methods with something more flexible.
+    # I can get the list of methods using reflection, but how do I ensure that any operations won't delete the root directory?
+    @methods = (["insert", "include?", "gsub", "size", "replace", "to_i", "chomp!", "succ", "oct", "to_s", "rstrip", "taguri=", "lines", "capitalize!", "hash", "capitalize", "center", "*", "index", "crypt", "+", "=~", "strip", "each_byte", "gsub!", "count", "delete!", "upcase", "ljust", "delete", "is_binary_data?", "upcase!", "rstrip!", "sum", "eql?", "start_with?", "to_sym", "length", "chop", "to_yaml", "to_f", "tr!", "to_str", "[]", "unpack", "tr", "inspect", "bytes", "strip!", "[]=", "slice!", "split", "sub", "each", "empty?", "swapcase!", "<<", "casecmp", "swapcase", "rindex", "intern", "rpartition", "reverse!", "next!", "lstrip", "hex", "chop!", "match", "each_char", "downcase!", "rjust", "downcase", "squeeze", "squeeze!", "concat", "upto", "end_with?", "slice", "chomp", "<=>", "bytesize", "sub!", "succ!", "each_line", "dump", "==", "scan", "tr_s", "tr_s!", "partition", "is_complex_yaml?", "next", "%", "reverse", "lstrip!", "chars", "taguri"] + ["%", "odd?", "prec_i", "<<", "div", "&", ">>", "lcm", "power!", "to_sym", "*", "ord", "+", "next", "round", "prec_f", "-", "even?", "denominator", "singleton_method_added", "divmod", "/", "integer?", "downto", "gcdlcm", "|", "gcd", "size", "truncate", "~", "to_i", "modulo", "zero?", "times", "to_r", "rdiv", "^", "+@", "-@", "quo", "**", "upto", "to_f", "<", "step", "numerator", "<=>", "between?", "remainder", ">", "to_int", "nonzero?", "pred", "coerce", "rpower", "floor", "succ", ">=", "prec", "<=", "fdiv", "abs", "chr", "id2name", "ceil", "[]"]).uniq
   end
-  method = methods[rand(methods.size)]
 
-  receiver_object = objects[receiver_object_index]
-  parameter_objects = objects.values_at(*parameters_indexes)
+  def create_task
+    receiver_object_index = rand(@objects.size)
+    num_parameters = rand(5)
+    parameters_indexes = []
+    num_parameters.times do
+      index = rand(@objects.size)
+      next if index == receiver_object_index
+      next if parameters_indexes.include?(index)
+      parameters_indexes << index
+    end
+    method = @methods[rand(@methods.size)]
+
+    receiver_object = @objects[receiver_object_index]
+    parameter_objects = @objects.values_at(*parameters_indexes)
+
+    task = SmallEigenCollider::Task.new(receiver_object, method, parameter_objects)
+    task
+  end
+end
+
+class SmallEigenCollider::Task
+  attr_reader :receiver_object, :method, :parameter_objects
+
+  def initialize(receiver_object, method, parameter_objects)
+    @receiver_object, @method, @parameter_objects = receiver_object, method, parameter_objects
+    @status = :not_run_yet
+  end
+
+  def run
+    begin
+      Timeout.timeout(2) do
+        @result = @receiver_object.send(@method, *@parameter_objects, &:consistent_inspect)
+        @status = :success
+      end
+    rescue Timeout::Error
+      @status = :timeout
+    rescue Exception
+      @status = :failure
+    end
+  end
+
+  def log_result(logger)
+    case @status
+    when :timeout, :failure
+      logger.log_failure(@receiver_object, @method, @parameter_objects)
+    when :success
+      logger.log_result(@result)
+    else raise
+    end
+  end
+end
+
+logger = SmallEigenCollider::Logger.new_using_filename(output_filename)
+task_creator = SmallEigenCollider::TaskCreator.new
+
+10000.times do
   # FIXME add a random block
 
   # FIXME rather than using Object#inspect, I have to create a method whose output doesn't vary depending on object id
   # or ruby implementation
 
   logger.log_start
-  logger.log_input_parameters(receiver_object, method, parameter_objects)
-  begin
-    Timeout.timeout(2) do
-      result = receiver_object.send(method, *parameter_objects, &:consistent_inspect)
-      logger.log_result(result)
-    end
-  rescue Exception => e
-    logger.log_failure(receiver_object, method, parameter_objects)
-  end
+  task = task_creator.create_task
+
+  logger.log_input_parameters(task)
+  task.run
+  task.log_result(logger)
   logger.log_end
 end
 logger.close
