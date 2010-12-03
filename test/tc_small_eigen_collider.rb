2e9ddeff7d1bf3c921cb9cb32234a5e5b7ae065f
Andrew Grimm <andrew.j.grimm@gmail.com>
2010-12-03 22:09:24 +1100

    Moved test helpers.

--- test/tc_small_eigen_collider.rb
+++ test/tc_small_eigen_collider.rb
@@ -2,6 +2,47 @@ $:.unshift File.join(File.dirname(__FILE__), "..", "lib")
 require "small_eigen_collider"
 require "test/unit"
 
+module TestSmallEigenColliderHelper
+  def assert_roundtrips(receiver_object, method_name, parameters, yaml_dump_filename)
+    File.delete(yaml_dump_filename) if File.exist?(yaml_dump_filename)
+
+    first_mock_filestream = StringIO.new
+    task = SmallEigenCollider::Task.new(receiver_object, method_name, parameters)
+    task_list = SmallEigenCollider::TaskList.new([task])
+    task_list.run_and_log_each_task(first_mock_filestream)
+    yaml_string = task_list.dump_tasks_to_yaml_string
+    task_list.dump_tasks_to_yaml(yaml_dump_filename)
+
+    second_mock_filestream = StringIO.new
+    yaml_created_task_list = SmallEigenCollider::TaskList.new_using_yaml(yaml_dump_filename)
+    yaml_created_task_list.run_and_log_each_task(second_mock_filestream)
+    second_yaml_string = yaml_created_task_list.dump_tasks_to_yaml_string
+    assert_equal yaml_string, second_yaml_string, "Side effect problems"
+    # FIXME gsubs are to avoid irrelevant details from producing false claims of difference.
+    # The optional 1 before the x is to handle JRuby - see JRuby bug 4977
+    assert_equal first_mock_filestream.string.gsub(/01?x[0-9abcdef]+/, "0xc0ffee"), second_mock_filestream.string.gsub(/01?x[0-9abcdef]+/, "0xc0ffee"), "Side effect problems"
+  end
+
+  def create_single_item_task_list(receiver_object, method_name, parameters)
+    raise ArgumentError unless parameters.is_a? Array
+    task = SmallEigenCollider::Task.new(receiver_object, method_name, parameters)
+    task_list = SmallEigenCollider::TaskList.new([task])
+    task_list
+  end
+
+  def task_list_output_empty?(task_list)
+    log_output_filestream = StringIO.new
+    task_list.run_and_log_each_task(log_output_filestream)
+    log_output_filestream.string.empty?
+  end
+
+  def task_yaml_output_empty?(task_list)
+    task_list_yaml = task_list.dump_tasks_to_yaml_string
+    yaml_created_task_list = SmallEigenCollider::TaskList.new_using_yaml_string(task_list_yaml)
+    yaml_created_task_list.empty?
+  end
+end
+
 class TestSecurity < Test::Unit::TestCase
 
   # Detection of security breaches would fail under JRuby, for any level of $SAFE
@@ -23,25 +64,7 @@ class TestProgramWorks < Test::Unit::TestCase
 end
 
 class TestRoundtripping < Test::Unit::TestCase
-  def assert_roundtrips(receiver_object, method_name, parameters, yaml_dump_filename)
-    File.delete(yaml_dump_filename) if File.exist?(yaml_dump_filename)
-
-    first_mock_filestream = StringIO.new
-    task = SmallEigenCollider::Task.new(receiver_object, method_name, parameters)
-    task_list = SmallEigenCollider::TaskList.new([task])
-    task_list.run_and_log_each_task(first_mock_filestream)
-    yaml_string = task_list.dump_tasks_to_yaml_string
-    task_list.dump_tasks_to_yaml(yaml_dump_filename)
-
-    second_mock_filestream = StringIO.new
-    yaml_created_task_list = SmallEigenCollider::TaskList.new_using_yaml(yaml_dump_filename)
-    yaml_created_task_list.run_and_log_each_task(second_mock_filestream)
-    second_yaml_string = yaml_created_task_list.dump_tasks_to_yaml_string
-    assert_equal yaml_string, second_yaml_string, "Side effect problems"
-    # FIXME gsubs are to avoid irrelevant details from producing false claims of difference.
-    # The optional 1 before the x is to handle JRuby - see JRuby bug 4977
-    assert_equal first_mock_filestream.string.gsub(/01?x[0-9abcdef]+/, "0xc0ffee"), second_mock_filestream.string.gsub(/01?x[0-9abcdef]+/, "0xc0ffee"), "Side effect problems"
-  end
+  include TestSmallEigenColliderHelper
 
   def test_roundtripping_works
     assert_roundtrips("a", "<<", "b", "test/data/simple_roundtrip.yml")
@@ -66,24 +89,7 @@ class TestRoundtripping < Test::Unit::TestCase
 end
 
 class TestFilter < Test::Unit::TestCase
-  def create_single_item_task_list(receiver_object, method_name, parameters)
-    raise ArgumentError unless parameters.is_a? Array
-    task = SmallEigenCollider::Task.new(receiver_object, method_name, parameters)
-    task_list = SmallEigenCollider::TaskList.new([task])
-    task_list
-  end
-
-  def task_list_output_empty?(task_list)
-    log_output_filestream = StringIO.new
-    task_list.run_and_log_each_task(log_output_filestream)
-    log_output_filestream.string.empty?
-  end
-
-  def task_yaml_output_empty?(task_list)
-    task_list_yaml = task_list.dump_tasks_to_yaml_string
-    yaml_created_task_list = SmallEigenCollider::TaskList.new_using_yaml_string(task_list_yaml)
-    yaml_created_task_list.empty?
-  end
+  include TestSmallEigenColliderHelper
 
   def test_filter
     bogus_task_task_list = create_single_item_task_list(1, "+", ["a"])
